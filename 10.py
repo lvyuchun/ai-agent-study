6c0d2b9a6641f5a4001768f207c96a2e05d2e4a5
lvyuchun <17385499224@163.com>
2026-08-18 19:16:47 +0800

    2

--- 10.py
+++ 10.py
@@ -1,2 +1,19 @@
+
+from collections import Counter
+from collections import namedtuple
+from collections import defaultdict
+
+dd = defaultdict(list)      # 默认值是空列表
+dd["水果"].append("苹果")    # 不需要先判断键在不在！
+dd["水果"].append("香蕉")
+# defaultdict(list, {'水果': ['苹果', '香蕉']})
+
 x=((x,y) for x in range(5) for y in range(5))
 print(list(x))
+k="aaaaavvvvvcccccsssss"
+print(Counter(k))
+
+point = namedtuple('Point', ['x', 'y', 'z'])
+pp = point(1, 2, 3)
+print(pp[0])
+print(pp.x)
